d412cdcccd87d6e83ff64c9b524d79bf9594aaab
Ebenezer Don <ebenezerdonu@gmail.com>
2020-09-23 05:06:42 +0100

    add query for getting courses

--- main.py
+++ main.py
@@ -5,8 +5,20 @@ from starlette.graphql import GraphQLApp
 from schemas import CourseType, InstructorType
 import json
 
+class Query(ObjectType):
+  course_list = None
+  get_course = Field(List(CourseType), id=String())
+
+  async def resolve_get_course(self, info, id=None):
+    with open("./courses.json") as courses:
+      course_list = json.load(courses)
+    if (id):
+      for course in course_list:
+        if course['id'] == id: return [course]
+    return course_list
+
 app = FastAPI()
 app.add_route("/", GraphQLApp(
-  schema=Schema(),
+  schema=Schema(query=Query),
   executor_class=AsyncioExecutor)
 )

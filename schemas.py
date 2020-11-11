8cef24dfbf57c6f8143788c74cec1f208a4ad0cc
Ebenezer Don <ebenezerdonu@gmail.com>
2020-11-11 03:56:30 +0100

    remove redundant types

--- schemas.py
+++ schemas.py
@@ -1,13 +1,7 @@
-from graphene import String, List, ObjectType
+from graphene import String, ObjectType
 
 class CourseType(ObjectType):
   id = String(required=True)
   title = String(required=True)
   instructor = String(required=True)
   publish_date = String()
-
-class InstructorType(ObjectType):
-  id = String(required=True)
-  name = String(required=True)
-  gender = String()
-  country = String()
